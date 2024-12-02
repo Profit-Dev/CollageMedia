189982e0e86497d06abf6c10de0a1b40f76ac727
RodazTheCoder <rodrigofhc41@gmail.com>
2024-12-02 11:47:58 -0300

    feat: create color palette

---
+++ src/main/kotlin/presentation/view/themes/Color.kt
@@ -0,0 +1,8 @@
+package presentation.view.themes
+
+import androidx.compose.ui.graphics.Color
+
+val mainWallpaperColor = Color(0xFF68C1EF)
+val secondaryColor = Color(0xFF006BA1)
+val whiteColor = Color(0xFFFFFFFF)
+val windowColor = Color(0xFFD5EEFA)
