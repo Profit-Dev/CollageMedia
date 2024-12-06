7cdc7e8b855dd0b518a5298b7007d99169f04ac4
RodazTheCoder <rodrigofhc41@gmail.com>
2024-12-06 10:32:53 -0300

    feat: set window minimum size

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,22 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import presentation.view.screens.MainScreen
+import presentation.view.themes.CollageMediaTheme
+import java.awt.Dimension
 
 @Composable
 @Preview
 fun App() {
+    CollageMediaTheme {
+        MainScreen()
+    }
 }
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
+        window.minimumSize = Dimension(985,600)
         App()
     }
 }
