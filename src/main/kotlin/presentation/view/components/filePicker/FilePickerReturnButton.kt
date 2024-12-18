ef414b10aa02b1863c3b8d3281ad2cd3b8a76403
RodazTheCoder <rodrigofhc41@gmail.com>
2024-12-18 11:07:12 -0300

    refactor: change Button to FloatingActionButton and change to ArrowBack icon

--- src/main/kotlin/presentation/view/components/filePicker/FilePickerReturnButton.kt
+++ src/main/kotlin/presentation/view/components/filePicker/FilePickerReturnButton.kt
@@ -1,37 +1,30 @@
 package presentation.view.components.filePicker
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import presentation.view.themes.mainWallpaperColor
 import presentation.view.themes.whiteColor
 
 @Composable
-fun FilePickerReturnkButton(modifier: Modifier = Modifier, onClick: (() -> Unit)? = null) {
-    Button(
+fun FilePickerReturnButton(modifier: Modifier = Modifier, onClick: (() -> Unit)? = null) {
+    FloatingActionButton(
         onClick = { onClick?.invoke() },
-        modifier = modifier
-            .height(80.dp)
-            .width(80.dp),
+        modifier = modifier,
         shape = CircleShape,
-        colors = ButtonDefaults.buttonColors(backgroundColor = mainWallpaperColor, contentColor = whiteColor)
+        backgroundColor = mainWallpaperColor, contentColor = whiteColor
     ) {
-        Icon(Icons.Filled.ArrowBackIosNew, "nav-before", modifier = Modifier.size(100.dp))
+        Icon(Icons.AutoMirrored.Filled.ArrowBack, "nav-before")
     }
 }
 
 @Composable
 @Preview
 fun ViewButton2() {
-    FilePickerReturnkButton { }
+    FilePickerReturnButton { }
 }
