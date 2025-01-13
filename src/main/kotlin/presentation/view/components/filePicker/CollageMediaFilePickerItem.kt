b7b027710997f487f63f006bcf30423a07d3e43d
Kaue Miziara <kauemiziara@gmail.com>
2025-01-13 10:04:57 -0300

    update: replace picture icon with `AsyncImage` displaying the images in `CollageMediaFilePickerItem`

--- src/main/kotlin/presentation/view/components/filePicker/CollageMediaFilePickerItem.kt
+++ src/main/kotlin/presentation/view/components/filePicker/CollageMediaFilePickerItem.kt
@@ -8,13 +8,13 @@ import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Folder
-import androidx.compose.material.icons.filled.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import models.files.FilePickerFile
 import presentation.view.themes.collageLightBlueColor
 import presentation.view.themes.mainWallpaperColor
@@ -37,10 +37,9 @@ fun CollageMediaFilePickerItem(item: FilePickerFile, isSelected: Boolean, boxSiz
                 modifier = Modifier.weight(1f),
             )
         } else {
-            Icon(
-                imageVector = Icons.Default.Image,
-                "File",
-                modifier = Modifier.weight(1f),
+            AsyncImage(
+                model = item.path,
+                contentDescription = item.name,
             )
         }
 
