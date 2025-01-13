35ac80ca91fc6b2a977765b50cfacede0d84ac70
Kaue Miziara <kauemiziara@gmail.com>
2025-01-13 09:14:43 -0300

    update: add `path` string property to `FilePickerFile` data class

--- src/main/kotlin/models/files/FilePickerFile.kt
+++ src/main/kotlin/models/files/FilePickerFile.kt
@@ -7,15 +7,17 @@ import java.io.File
  *
  * @property name The name of the file.
  * @property isDir Boolean indicating whether the file is a directory.
+ * @property path The file absolute path.
  */
 data class FilePickerFile(
     val name: String,
     val isDir: Boolean,
+    val path: String,
 ) {
     /**
      * Creates a `FilePickerFile` from a `File` object.
      *
      * @param file the `File` object to create the `FilePickerFile` from.
      */
-    constructor(file: File) : this(name = file.name, isDir = file.isDirectory)
+    constructor(file: File) : this(name = file.name, isDir = file.isDirectory, path = file.absolutePath)
 }
