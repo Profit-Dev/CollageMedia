4247c0017d78dbb82952b34aea75f2b71ea1d968
Kaue Miziara <kauemiziara@gmail.com>
2025-01-21 09:34:09 -0300

    change: sort directories and files returned by `filterDirectoriesAndPictures`

--- src/main/kotlin/extensions/fileExtensions.kt
+++ src/main/kotlin/extensions/fileExtensions.kt
@@ -7,7 +7,7 @@ fun Array<File>?.filterDirectoriesAndPictures(): List<File>? = this?.filter { f 
     f.isDirectory or (f.isFile && f.extension.lowercase(Locale.getDefault()) in arrayOf(
         "png", "jpg", "jpeg"
     ))
-}?.toList()
+}?.toList()?.sortedWith(compareBy({ !it.isDirectory }, { it.name }))
 
 fun File.listFilesInDirectory(): List<File> = try {
     this.listFiles()?.filterDirectoriesAndPictures() ?: emptyList()
