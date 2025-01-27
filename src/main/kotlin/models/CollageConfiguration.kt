d29baa9127bd83e7e8391930cc758dc035af1b4e
Kaue Miziara <kauemiziara@gmail.com>
2025-01-27 11:19:47 -0300

    update: add `rows` and `columns` properties to `CollageConfiguration` data class

--- src/main/kotlin/models/CollageConfiguration.kt
+++ src/main/kotlin/models/CollageConfiguration.kt
@@ -4,9 +4,15 @@ package models
  * Data class representing the configurations for a collage.
  *
  * @property pictureOrientation The orientation of the pictures in the collage.
+ * @property rows The number of picture rows to be displayed displayed per page.
+ * @property columns The number of picture columns to be displayed displayed per page.
  * @property imagesPerPage The number of images to be displayed per page.
+ *
+ * @author Kauê Miziara
  */
 data class CollageConfiguration(
     val pictureOrientation: PictureOrientation,
-    val imagesPerPage: Int,
+    val rows: Int,
+    val columns: Int,
+    val imagesPerPage: Int = rows * columns,
 )
